d3b19280088cafe0f96015eefc48f671ef0a393b
Naeun <knae11@naver.com>
2020-12-07 14:38:07 +0900

    Check valid value of input
    Validator checks length of input line, valid values of parsed input. It throws IllegalArgumentException.

--- src/PushingWords.java
+++ src/PushingWords.java
@@ -3,19 +3,41 @@ import java.util.Scanner;
 public class PushingWords {
 
     private final Scanner scanner;
+    private final Validator validator;
+    private String word;
+    private int count;
+    private String direction;
+
     public PushingWords(Scanner scanner){
         this.scanner = scanner;
+        validator = new Validator();
         init();
     }
 
     private void init() {
         while(true){
+            System.out.print(">");
             String userLine = scanner.nextLine().trim();
             if(userLine.equals("q")){
                 return;
             }
-            System.out.println(userLine);
+            parseUserLine(userLine);
+            printResult();
         }
+    }
+
+    private void printResult() {
+    }
+
+    private void parseUserLine(String line) {
+        String[] parsedLine = line.split(" ");
+        validator.checkLineLength(parsedLine);
+        validator.checkNumber(parsedLine[1]);
+        validator.checkDirection(parsedLine[2]);
+        word = parsedLine[0];
+        count = Integer.parseInt(parsedLine[1]);
+        direction = parsedLine[2];
+
     }
 
 
---
+++ src/Validator.java
@@ -0,0 +1,26 @@
+public class Validator {
+    public Validator(){
+
+    }
+
+    public void checkLineLength(String[] parsedLine){
+        if(parsedLine.length >3){
+            throw new IllegalArgumentException("공백을 기준으로 `단어 숫자 방향`을 입력해 주세요.");
+        }
+    }
+    public void checkNumber(String number){
+        try{
+            int num = Integer.parseInt(number);
+            if(-100>num || 99<num){
+                throw new IllegalArgumentException("두번째 값으로 -100이상 100미만의 숫자를 입력해 주세요.");
+            }
+        }catch (NumberFormatException n){
+            throw new IllegalArgumentException("두번째 값으로 숫자를 입력해 주세요.");
+        }
+    }
+    public void checkDirection(String direction){
+        if(!direction.toLowerCase().equals("r")&& !direction.toLowerCase().equals("l")){
+            throw new IllegalArgumentException("세번째 값으로 r,l,R,L 중 하나를 입력해 주세요");
+        }
+    }
+}
