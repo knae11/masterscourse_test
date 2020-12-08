15982122bc9b4c9a0d901c7cd91865e1e1ae4122
Naeun <knae11@naver.com>
2020-12-08 13:19:50 +0900

    Close scanner

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         new CubeSystem(scanner);
+        scanner.close();
     }
 }
