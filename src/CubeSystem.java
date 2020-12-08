eb8b0f50050c138c258bc69f646371c34ebade6e
Naeun <knae11@naver.com>
2020-12-08 14:14:22 +0900

    Complete cube system
    Handle left line and the other cases

--- src/CubeSystem.java
+++ src/CubeSystem.java
@@ -38,19 +38,92 @@ public class CubeSystem {
 
     }
 
+    //TODO: step-1처럼 boolean 값으로 반대방향 지정해서 가능 할 것 같음 ( 중복 로직제거하기 )
     private void handleCube(String round) {
+        String movedLine;
         switch (round) {
             case "L":
             case "L'":
-                String movedLine = moveLeft(round);
+                movedLine = moveLeft(round);
                 setLeftLineCube(movedLine);
                 break;
+            case "R":
+            case "R'":
+                movedLine = moveRight(round);
+                setRightLineCube(movedLine);
+                break;
+            case "U":
+            case "U'":
+                movedLine = moveTop(round);
+                setTopLineCube(movedLine);
+                break;
+            case "B":
+            case "B'":
+                movedLine = moveBottom(round);
+                setBottomLineCube(movedLine);
+                break;
+            default:
+                break;
+
 
         }
     }
 
+    private String moveTop(String round) {
+        String leftLine = "";
+        for (int i = 0; i < 3; i++) {
+            leftLine += cube[0][i];
+        }
+        if (round.contains("'")) {
+            return leftLine.substring(2, 3) + leftLine.substring(0, 2);
+        }
+
+        return leftLine.substring(1, 3) + leftLine.substring(0, 1);
+    }
+
+    private void setTopLineCube(String movedLine) {
+        for (int i = 0; i < 3; i++) {
+            cube[0][i] = movedLine.charAt(i);
+        }
+    }
+
+    private String moveBottom(String round) {
+        String leftLine = "";
+        for (int i = 0; i < 3; i++) {
+            leftLine += cube[2][i];
+        }
+        if (round.contains("'")) {
+            return leftLine.substring(1, 3) + leftLine.substring(0, 1);
+        }
+        return leftLine.substring(2, 3) + leftLine.substring(0, 2);
+    }
+
+    private void setBottomLineCube(String movedLine) {
+        for (int i = 0; i < 3; i++) {
+            cube[2][i] = movedLine.charAt(i);
+        }
+
+    }
+
+    private String moveRight(String round) {
+        String leftLine = "";
+        for (int i = 0; i < 3; i++) {
+            leftLine += cube[i][2];
+        }
+        if (round.contains("'")) {
+            return leftLine.substring(2, 3) + leftLine.substring(0, 2);
+        }
+        return leftLine.substring(1, 3) + leftLine.substring(0, 1);
+    }
+
+    private void setRightLineCube(String movedLine) {
+        for (int i = 0; i < 3; i++) {
+            cube[i][2] = movedLine.charAt(i);
+        }
+    }
+
     private void setLeftLineCube(String movedLine) {
-        for(int i = 0; i<3; i++){
+        for (int i = 0; i < 3; i++) {
             cube[i][0] = movedLine.charAt(i);
         }
     }
@@ -61,7 +134,7 @@ public class CubeSystem {
             leftLine += cube[i][0];
         }
         if (round.contains("'")) {
-            return  leftLine.substring(1, 3) + leftLine.substring(0, 1);
+            return leftLine.substring(1, 3) + leftLine.substring(0, 1);
         }
         return leftLine.substring(2, 3) + leftLine.substring(0, 2);
 
