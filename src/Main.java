0e6894dfc4cc664ef3aeafa844fb5fc00c248a93
Naeun <knae11@naver.com>
2020-12-08 22:58:04 +0900

    Implement ending message with spending time

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        final long start = System.currentTimeMillis();
         RubikCube rubikCube = new RubikCube(scanner);
+        final long end = System.currentTimeMillis();
         scanner.close();
-        System.out.println("경과시간: ");
+        final int time = (int)((end-start)/1000);
+
+        System.out.printf("경과시간: %02d:%02d\n", time/60, time%60);
         System.out.println("조작갯수: "+rubikCube.getCounter());
         System.out.println("이용해주셔서 감사합니다. 뚜뚜뚜.");
     }
