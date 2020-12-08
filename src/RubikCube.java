a857b4ec2ce7d7c29817c90df0ecb5423e8d2b34
Naeun <knae11@naver.com>
2020-12-08 19:50:54 +0900

    Implement clockwise conditions

--- src/RubikCube.java
+++ src/RubikCube.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -29,13 +31,88 @@ public class RubikCube {
     }
 
     private void system(String input) {
-        StringBuilder line = new StringBuilder();
-        if (input.equals("F")) {
-            line.append(rubikCube.get(Faces.TOP).getTopAndBottomLine(2));
-            line.append(rubikCube.get(Faces.RIGHT).getLeftAndRightLine(0));
-            line.append(rubikCube.get(Faces.BOTTOM).getTopAndBottomLine(0));
-            line.append(rubikCube.get(Faces.LEFT).getLeftAndRightLine(2));
+        switch (input) {
+            case "F":
+                moveFront();
+                break;
+            case "U":
+                moveTop();
+                break;
+            case "D":
+                moveBottom();
+                break;
+            case "B":
+                moveBack();
+                break;
+            case "L":
+                moveLeft();
+                break;
+            case "R":
+                moveRight();
+                break;
+            default:
+                break;
         }
+    }
+
+    private void moveRight() {
+        StringBuilder line = new StringBuilder();
+        line.append(rubikCube.get(Faces.TOP).getLeftAndRightLine(2));
+        line.append(rubikCube.get(Faces.BACK).getLeftAndRightLine(0));
+        line.append(rubikCube.get(Faces.BOTTOM).getLeftAndRightLine(2));
+        line.append(rubikCube.get(Faces.FRONT).getLeftAndRightLine(2));
+        String newLine = line.substring(9) + line.substring(0, 9);
+        rubikCube.get(Faces.TOP).setLeftAndRightCube(2, newLine.substring(0, 3));
+        rubikCube.get(Faces.BACK).setLeftAndRightCube(0, newLine.substring(3, 6));
+        rubikCube.get(Faces.BOTTOM).setLeftAndRightCube(2, newLine.substring(6, 9));
+        rubikCube.get(Faces.FRONT).setLeftAndRightCube(2, newLine.substring(9));
+    }
+
+    private void moveLeft() {
+        StringBuilder line = new StringBuilder();
+        line.append(rubikCube.get(Faces.TOP).getLeftAndRightLine(0));
+        line.append(rubikCube.get(Faces.FRONT).getLeftAndRightLine(0));
+        line.append(rubikCube.get(Faces.BOTTOM).getLeftAndRightLine(0));
+        line.append(rubikCube.get(Faces.BACK).getLeftAndRightLine(2));
+        String newLine = line.substring(9) + line.substring(0, 9);
+        rubikCube.get(Faces.TOP).setLeftAndRightCube(0, newLine.substring(0, 3));
+        rubikCube.get(Faces.FRONT).setLeftAndRightCube(0, newLine.substring(3, 6));
+        rubikCube.get(Faces.BOTTOM).setLeftAndRightCube(0, newLine.substring(6, 9));
+        rubikCube.get(Faces.BACK).setLeftAndRightCube(2, newLine.substring(9));
+    }
+
+    private void moveBottom() {
+        StringBuilder line = new StringBuilder();
+        line.append(rubikCube.get(Faces.FRONT).getTopAndBottomLine(2));
+        line.append(rubikCube.get(Faces.RIGHT).getTopAndBottomLine(2));
+        line.append(rubikCube.get(Faces.BACK).getTopAndBottomLine(2));
+        line.append(rubikCube.get(Faces.LEFT).getTopAndBottomLine(2));
+        String newLine = line.substring(9) + line.substring(0, 9);
+        rubikCube.get(Faces.FRONT).setTopAndBottomCube(2, newLine.substring(0, 3));
+        rubikCube.get(Faces.RIGHT).setTopAndBottomCube(2, newLine.substring(3, 6));
+        rubikCube.get(Faces.BACK).setTopAndBottomCube(2, newLine.substring(6, 9));
+        rubikCube.get(Faces.LEFT).setTopAndBottomCube(2, newLine.substring(9));
+    }
+
+    private void moveTop() {
+        StringBuilder line = new StringBuilder();
+        line.append(rubikCube.get(Faces.FRONT).getTopAndBottomLine(0));
+        line.append(rubikCube.get(Faces.LEFT).getTopAndBottomLine(0));
+        line.append(rubikCube.get(Faces.BACK).getTopAndBottomLine(0));
+        line.append(rubikCube.get(Faces.RIGHT).getTopAndBottomLine(0));
+        String newLine = line.substring(9) + line.substring(0, 9);
+        rubikCube.get(Faces.FRONT).setTopAndBottomCube(0, newLine.substring(0, 3));
+        rubikCube.get(Faces.LEFT).setTopAndBottomCube(0, newLine.substring(3, 6));
+        rubikCube.get(Faces.BACK).setTopAndBottomCube(0, newLine.substring(6, 9));
+        rubikCube.get(Faces.RIGHT).setTopAndBottomCube(0, newLine.substring(9));
+    }
+
+    private void moveFront() {
+        StringBuilder line = new StringBuilder();
+        line.append(rubikCube.get(Faces.TOP).getTopAndBottomLine(2));
+        line.append(rubikCube.get(Faces.RIGHT).getLeftAndRightLine(0));
+        line.append(rubikCube.get(Faces.BOTTOM).getTopAndBottomLine(0));
+        line.append(rubikCube.get(Faces.LEFT).getLeftAndRightLine(2));
         String newLine = line.substring(9) + line.substring(0, 9);
         rubikCube.get(Faces.TOP).setTopAndBottomCube(2, newLine.substring(0, 3));
         rubikCube.get(Faces.RIGHT).setLeftAndRightCube(0, newLine.substring(3, 6));
@@ -43,9 +120,22 @@ public class RubikCube {
         rubikCube.get(Faces.LEFT).setLeftAndRightCube(2, newLine.substring(9));
     }
 
+    private void moveBack() {
+        StringBuilder line = new StringBuilder();
+        line.append(rubikCube.get(Faces.TOP).getTopAndBottomLine(0));
+        line.append(rubikCube.get(Faces.LEFT).getLeftAndRightLine(0));
+        line.append(rubikCube.get(Faces.BOTTOM).getTopAndBottomLine(2));
+        line.append(rubikCube.get(Faces.RIGHT).getLeftAndRightLine(2));
+        String newLine = line.substring(9) + line.substring(0, 9);
+        rubikCube.get(Faces.TOP).setTopAndBottomCube(0, newLine.substring(0, 3));
+        rubikCube.get(Faces.LEFT).setLeftAndRightCube(0, newLine.substring(3, 6));
+        rubikCube.get(Faces.BOTTOM).setTopAndBottomCube(2, newLine.substring(6, 9));
+        rubikCube.get(Faces.RIGHT).setLeftAndRightCube(2, newLine.substring(9));
+    }
+
 
     private void initRubik() {
-        for(Faces face : Faces.values()){
+        for (Faces face : Faces.values()) {
             rubikCube.put(face, new CubeFace(face.getColor()));
         }
     }
